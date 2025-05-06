dff1c09099ec864045b20898180eadcb1a1034a5
jacobadam <jacobanevitt@gmail.com>
2025-05-06 13:40:06 -0600

    remove logs

--- src/app/components/Background.jsx
+++ src/app/components/Background.jsx
@@ -15,10 +15,8 @@ export default function Background() {
 const BGGrid = () => {
   const { resolvedTheme } = useTheme();
 
-  console.log(resolvedTheme);
   const isDarkMode = resolvedTheme === "dark";
 
-  console.log(isDarkMode);
   const lightModeGrid = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='1' stroke='rgba(0, 0, 0, 0.03)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`;
   const darkModeGrid = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='rgb(30 27 75 / 0.5)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`;
 

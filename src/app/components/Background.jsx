44f3f017a40b328f34f8b99f0384d34a214f6ce3
jacobadam <jacobanevitt@gmail.com>
2025-05-06 13:25:52 -0600

    use resolvedTheme to test

--- src/app/components/Background.jsx
+++ src/app/components/Background.jsx
@@ -13,10 +13,10 @@ export default function Background() {
 }
 
 const BGGrid = () => {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
 
-  console.log(theme);
-  const isDarkMode = theme === "dark";
+  console.log(resolvedTheme);
+  const isDarkMode = resolvedTheme === "dark";
 
   console.log(isDarkMode);
   const lightModeGrid = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='1' stroke='rgba(0, 0, 0, 0.03)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`;

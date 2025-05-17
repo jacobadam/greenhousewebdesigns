8326ccda76fbde87abf41874afd71d9092b582eb
jacobadam <jacobanevitt@gmail.com>
2025-05-17 10:49:20 -0600

    inject meta using next/hed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import Background from "./components/Background";
 import ThemeProvider from "./theme/ThemeContext";
 import { Analytics } from "@vercel/analytics/next";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -80,23 +81,51 @@ export async function generateMetadata() {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <>
+      <Head>
+        <meta property="og:title" content="Greenhouse Web Designs" />
+        <meta
+          property="og:description"
+          content="Custom websites built for growth."
+        />
+        <meta
+          property="og:image"
+          content="https://www.greenhousewebdesigns.com/webdesign.webp"
+        />
+        <meta
+          property="og:url"
+          content="https://www.greenhousewebdesigns.com"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Greenhouse Web Designs" />
+        <meta
+          name="twitter:description"
+          content="Custom websites built for growth."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.greenhousewebdesigns.com/webdesign.webp"
+        />
+      </Head>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
         >
-          <Background />
-          <Nav />
-          <main>{children}</main>
-          <Analytics />
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Background />
+            <Nav />
+            <main>{children}</main>
+            <Analytics />
+            <Footer />
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }

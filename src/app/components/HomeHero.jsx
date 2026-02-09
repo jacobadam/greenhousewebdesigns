2dfa6970e603e70287e41b4822dc6a0d7d4b9204
jacobadam <jacobanevitt@gmail.com>
2026-02-09 14:46:49 -0600

    update tailwind classes

--- src/app/components/HomeHero.jsx
+++ src/app/components/HomeHero.jsx
@@ -13,7 +13,7 @@ export default function HomepageHero() {
         paddingBottom: "0px",
       }}
     >
-      <div className="h-full flex flex-col lg:flex-row items-center justify-center relative max-w-screen-xl mx-auto px-4 pb-16 md:pb-0 md:px-8">
+      <div className="h-full flex flex-col lg:flex-row items-center justify-center relative max-w-7xl mx-auto px-4 pb-16 md:pb-0 md:px-8">
         <div className="flex flex-col items-center justify-center px-4 text-center md:max-w-xl mx-auto">
           <motion.h1
             className="flex flex-row flex-wrap justify-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-black dark:text-white font-extrabold"
@@ -51,21 +51,21 @@ export default function HomepageHero() {
             <a
               href="/services"
               aria-label="Go to services"
-              className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border border-lightmode dark:border-darkmode bg-lightmode dark:bg-darkmode px-3 text-zinc-100 dark:text-black dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-500 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
+              className="flex justify-center items-center relative h-12.5 w-40 overflow-hidden border border-lightmode dark:border-darkmode bg-lightmode dark:bg-darkmode px-3 text-zinc-100 dark:text-black dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-500 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
             >
               <span className="relative z-3">Services</span>
             </a>
             <a
               href="/contact"
               aria-label="Go to contact page"
-              className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border dark:border-darkmode border-lightmode dark:hover:border-darkmode bg-transparent px-3 dark:text-darkmode text-lightmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-300 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
+              className="flex justify-center items-center relative h-12.5 w-40 overflow-hidden border dark:border-darkmode border-lightmode dark:hover:border-darkmode bg-transparent px-3 dark:text-darkmode text-lightmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-300 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
             >
               <span className="relative z-3">Get a Quote</span>
             </a>
           </div>
         </div>
 
-        <div className="order-first lg:order-none mb-12 lg:mb-0 lg:ml-12 flex justify-center relative z-2">
+        <div className="order-first lg:order-0 mb-12 lg:mb-0 lg:ml-12 flex justify-center relative z-2">
           <Image
             src="hero-image.svg"
             className="w-3/4 md:w-2/4 lg:w-full dark:hidden"
--- src/app/components/HomeServices.jsx
+++ src/app/components/HomeServices.jsx
@@ -3,7 +3,7 @@ import HomeServicesCard from "./HomeServicesCard";
 export default function HomeServices() {
   return (
     <section className="w-full h-full py-24 md:py-0 flex items-center justify-center bg-zinc-200 dark:bg-zinc-950">
-      <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-4">
+      <div className="max-w-7xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-4">
         <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
           <div className="lg:w-4/5 flex flex-col items-center justify-center mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
             <h1 className="text-2xl xl:text-3xl font-bold mb-2 text-black dark:text-white">
@@ -16,7 +16,7 @@ export default function HomeServices() {
             <a
               href="/services"
               aria-label="Go to services"
-              className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border dark:border-darkmode border-lightmode dark:hover:border-darkmode bg-lightmode dark:bg-transparent px-3 text-white dark:text-darkmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-300 hover:text-white hover:shadow-lightmode dark:hover:shadow-black hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
+              className="flex justify-center items-center relative h-12.5 w-40 overflow-hidden border dark:border-darkmode border-lightmode dark:hover:border-darkmode bg-lightmode dark:bg-transparent px-3 text-white dark:text-darkmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-300 hover:text-white hover:shadow-lightmode dark:hover:shadow-black hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
             >
               <span className="relative z-3">Services</span>
             </a>

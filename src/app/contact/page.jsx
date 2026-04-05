b3c1d5fbb90cd7d1d219b12bce767f0a96ff24fd
jacobadam <jacobanevitt@gmail.com>
2026-04-04 22:46:25 -0500

    update padding and styling

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useRef, useState } from "react";
+import Button from "../ui/Button";
 
 export default function Contact() {
   const form = useRef();
@@ -36,21 +37,15 @@ export default function Contact() {
   };
 
   return (
-    <section
-      className="py-16 bg-transparent flex-grow z-10"
-      style={{
-        paddingTop: "clamp(12rem, 10vw, 8rem)",
-        minHeight: "calc(100vh - clamp(4rem, 10vw, 8rem))",
-      }}
-    >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 z-20">
+    <section className="py-16 bg-transparent grow z-10 pt-28">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 lg:py-24 z-20">
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 lg:items-start items-center lg:justify-start justify-center">
           <div className="flex flex-col items-center text-center px-4 sm:px-6">
-            <h2 className="text-black dark:text-white text-2xl lg:text-3xl font-semibold leading-snug pb-5">
+            <h2 className="text-black dark:text-white text-3xl md:text-4xl xl:text-5xl font-semibold leading-snug pb-5">
               Contact
             </h2>
-            <p className="text-zinc-900 dark:text-zinc-100 text-md font-normal pb-6">
-              Get in touch and let's discuss how I can serve your business.
+            <p className="text-zinc-900 dark:text-zinc-100 text-base font-normal pb-6">
+              Get in touch and let&apos;s discuss how I can serve your business.
             </p>
             <div className="py-6 border-t border-gray-400 dark:border-zinc-200 text-center">
               <div className="flex flex-col items-center">
@@ -76,13 +71,13 @@ export default function Contact() {
                 </div>
 
                 <p className="text-zinc-800 dark:text-zinc-200 text-xs font-normal">
-                  (+1) 236-885-4130
+                  (+1) 672-888-0185
                 </p>
               </div>
             </div>
           </div>
 
-          <div className="p-4 rounded-lg">
+          <div className="px-4 rounded-lg">
             {messageSent && (
               <p
                 className="text-center text-lightmode dark:text-indigo-600 text-xl font-bold"
@@ -197,12 +192,7 @@ export default function Contact() {
                 />
               </div>
 
-              <button
-                type="submit"
-                className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border border-lightmode dark:border-darkmode bg-lightmode dark:bg-transparent px-3 text-zinc-100 dark:text-darkmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-500 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
-              >
-                <span className="relative z-3">Submit</span>
-              </button>
+              <Button href="/services">Submit</Button>
             </form>
           </div>
         </div>

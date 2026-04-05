4c426c8c9cd389a4548e78dc5631730e51a989ca
jacobadam <jacobanevitt@gmail.com>
2026-04-04 21:28:11 -0500

    update padding and text size, import and use button

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import Image from "next/image";
+import Button from "../ui/Button";
 
 export default function About() {
   return (
-    <section
-      className="py-16 relative"
-      style={{
-        paddingTop: "clamp(8rem, 10vw, 8rem)",
-        minHeight: "calc(100vh - clamp(4rem, 10vw, 8rem))",
-      }}
-    >
-      <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+    <section className="relative pt-28 lg:pt-8">
+      <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto py-12 lg:py-24">
         <div className="w-full flex-col justify-start items-start lg:gap-12 gap-10 inline-flex">
           <div className="w-full flex-col justify-start items-center gap-3 flex">
-            <h2 className="text-center text-black dark:text-white text-2xl lg:text-3xl font-bold font-manrope leading-normal">
+            <h1 className="text-center text-black dark:text-white text-3xl md:text-4xl xl:text-5xl font-bold font-manrope leading-normal">
               About Us
-            </h2>
-            <p className="max-w-xl text-center text-zinc-900 dark:text-zinc-100 text-base font-normal leading-relaxed">
+            </h1>
+            <p className="max-w-xl text-center text-zinc-900 dark:text-zinc-100 text-lg font-normal leading-relaxed">
               At Greenhouse Web Designs, we're passionate about building custom
               websites that help small businesses thrive online.
             </p>
@@ -31,7 +26,7 @@ export default function About() {
               priority
             />
             <div className="flex-col justify-center lg:items-start items-center gap-10 lg:gap-0 inline-flex">
-              <div className="flex-col justify-center items-start gap-3 flex">
+              <div className="flex-col justify-center items-start gap-3 flex mb-8">
                 <p className="text-zinc-800 dark:text-zinc-200 text-base font-normal leading-relaxed lg:text-start text-center">
                   I'm Jacob, founder of Greenhouse Web Designs — a small studio
                   with a big mission: helping businesses grow with high-quality,
@@ -57,13 +52,7 @@ export default function About() {
                   that works as hard as you do, I’d love to hear from you.
                 </p>
               </div>
-              <a
-                href="/contact"
-                aria-label="Go to contact page"
-                className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border border-lightmode dark:border-darkmode bg-lightmode dark:bg-transparent px-3 text-zinc-100 dark:text-darkmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-500 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-8"
-              >
-                <span className="relative z-3">Let's Talk</span>
-              </a>
+              <Button href="/contact">Let's Talk</Button>
             </div>
           </div>
         </div>

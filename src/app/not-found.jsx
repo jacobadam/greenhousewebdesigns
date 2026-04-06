0b204620bb9990992e8d7090276cac25e477af10
jacobadam <jacobanevitt@gmail.com>
2026-04-06 14:51:01 +0100

    import and use link button

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,8 @@
+import LinkButton from "./ui/LinkButton";
+
 export default function NotFound() {
   return (
-    <main className="grid min-h-full place-items-center bg-transparent px-6 py-24 sm:py-32 lg:px-8 flex-grow z-10">
+    <main className="grid min-h-full place-items-center bg-transparent px-6 py-24 sm:py-32 lg:px-8 grow z-10">
       <div className="text-center">
         <p className="text-xl font-semibold text-lightmode dark:text-darkmode">
           404
@@ -9,15 +11,12 @@ export default function NotFound() {
           Page not found
         </h1>
         <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8 dark:text-white">
-          Sorry, we couldn’t find the page you’re looking for.
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/"
-            className="flex justify-center items-center relative h-[50px] w-40 overflow-hidden border dark:border-darkmode border-lightmode dark:hover:border-darkmode bg-transparent px-3 dark:text-darkmode text-lightmode dark:hover:text-white hover:shadow-2xl before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-lightmode dark:before:bg-darkmode before:transition-all before:duration-300 hover:text-white hover:shadow-lightmode dark:hover:shadow-darkmode hover:before:left-0 hover:before:w-full rounded-full font-semibold mt-4"
-          >
-            <span className="relative z-3">Back to home</span>
-          </a>
+          <LinkButton href="/" width="w-40">
+            Back to Home
+          </LinkButton>
         </div>
       </div>
     </main>
